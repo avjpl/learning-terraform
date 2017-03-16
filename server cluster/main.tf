051b88441a9ba9b7f381e316856ad34e2c7dbc26
Adrian Lawrence <avjpldeveloper@aol.com>
2017-03-16 19:16:15 +0000

    Update to resolve error

--- server cluster/main.tf
+++ server cluster/main.tf
@@ -7,9 +7,9 @@ provider "aws" {
 }
 
 resource "aws_launch_configuration" "example" {
-  ami                    = "ami-ede2e889"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  image_id        = "ami-ede2e889"
+  instance_type   = "t2.micro"
+  security_groups = ["${aws_security_group.instance.id}"]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -18,11 +18,7 @@ resource "aws_launch_configuration" "example" {
               EOF
 
   lifecycle {
-    create_before_destory = true
-  }
-
-  tags {
-    Name = "terraform-example"
+    create_before_destroy = true
   }
 }
 
@@ -37,15 +33,15 @@ resource "aws_security_group" "instance" {
   }
 
   lifecycle {
-    create_before_destory = true
+    create_before_destroy = true
   }
 }
 
 resource "aws_autoscaling_group" "example" {
-  launch_confirguration = "${aws_launch_configuration.example.id}"
-  availablility_zones   = ["${data.aws_availability_zones.all.names}"]
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones   = ["${data.aws_availability_zones.all.names}"]
 
-  load_balancer     = ["${aws_elb.example.name}"]
+  load_balancers    = ["${aws_elb.example.name}"]
   health_check_type = "ELB"
 
   min_size = 2
@@ -58,10 +54,12 @@ resource "aws_autoscaling_group" "example" {
   }
 }
 
+data "aws_availability_zones" "all" {}
+
 resource "aws_elb" "example" {
-  name                = "terraform-asg-example"
-  availablility_zones = ["${data.aws_availability_zones.all.name}"]
-  security_groups     = ["${aws_security_group.elb.id}"]
+  name               = "terraform-asg-example"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  security_groups    = ["${aws_security_group.elb.id}"]
 
   listener {
     lb_port           = 80
@@ -71,26 +69,26 @@ resource "aws_elb" "example" {
   }
 
   health_check {
-    healthy_threshold  = 2
-    unhealthy_treshold = 2
-    timeout            = 3
-    interval           = 30
-    target             = "HTTP:${var.server_port}"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    interval            = 30
+    target              = "HTTP:${var.server_port}/"
   }
 }
 
 resource "aws_security_group" "elb" {
-  name = "terrform-example-elb"
+  name = "terraform-example-elb"
 
   ingress {
-    form_port   = 80
+    from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    form_port   = 80
+    from_port   = 80
     to_port     = 80
     protocol    = -1
     cidr_blocks = ["0.0.0.0/0"]
